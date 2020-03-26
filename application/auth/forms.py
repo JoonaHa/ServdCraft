ac5d0b0d536ca37a43f25cfcc831f803dfe5d02a
JoonaHa <joona.halonen@helsinki.fi>
2020-03-26 23:20:47 +0200

    Support for registering

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -3,8 +3,13 @@ from wtforms import PasswordField, StringField, validators
 
 
 class LoginForm(FlaskForm):
+    username = StringField("Username")
+    password = PasswordField("Password")
+
+class RegisterForm(FlaskForm):
+    name = StringField("Name",[validators.Length(min=2)])
     username = StringField("Username",[validators.Length(min=2)])
-    password = PasswordField("Password",[validators.Length(min=2)])
+    password = PasswordField("Password",[validators.Length(min=8)])
 
 #TODO remove
     class Meta:
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
 
 
 @app.route("/auth/login", methods=["GET", "POST"])
@@ -27,4 +27,27 @@ def auth_login():
 @app.route("/auth/logout")
 def auth_logout():
     logout_user()
-    return redirect(url_for("index"))    
+    return redirect(url_for("index"))
+
+
+@app.route("/auth/register", methods=["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registerform.html", form=RegisterForm())
+
+    form = RegisterForm(request.form)
+    if not form.validate():
+        return render_template("auth/registerform.html", form=form)
+
+    # Find if unique username
+    user = User.query.filter_by(username=form.username.data).first()
+
+    if user is None:
+        user = User(form.name.data, form.username.data, form.password.data)
+        db.session().add(user)
+        db.session().commit()
+        login_user(user)
+        return redirect(url_for("index"))
+    else:
+        return render_template("auth/registerform.html", form=form, error="Username already taken")
+
